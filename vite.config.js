f69abab82a53e8ca2546cbd5974adc2c0349ab2a
fengqichang <2155246533@stosystem.com>
2025-06-20 15:59:18 +0800

    fix: 打包配置

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  console.log(command);
+export default defineConfig(({ command, mode }) => {
+  console.log(command, mode);
   return {
-    base: command === "serve" ? "/" : "/pages/",
+    base: command === "serve" ? "/" : mode === "netlify" ? "/" : "/pages/",
     plugins: [react()],
   };
 });
